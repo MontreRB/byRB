07db3507420c409fe33c45ee18c600e723cfb9d0
MontreRB <148826173+MontreRB@users.noreply.github.com>
2023-10-28 21:38:25 +0100

    Update Formulaire.js

--- Formulaire.js
+++ Formulaire.js
@@ -26,6 +26,8 @@
   livraisonSelect.addEventListener("change", () => {
     const livraisonType = livraisonSelect.value;
     const wilayaSelectionne = wilayaSelect.value;
+     const selectedOption = selectElement.value;
+                let prix = "";
 
     // Définir les prix pour chaque wilaya lorsque "au stop agence" est choisi
     const prixParwilayaagence = {
