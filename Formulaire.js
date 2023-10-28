b8f57fdadbdc78d2f75aa4add9e2c085dc3392e0
MontreRB <148826173+MontreRB@users.noreply.github.com>
2023-10-28 22:57:36 +0100

    Update Formulaire.js

--- Formulaire.js
+++ Formulaire.js
@@ -155,6 +155,11 @@
     };
 
     // Mettre à jour la valeur du champ d'entrée du prix en fonction du choix
+
+   selectElement.addEventListener("change", function() {
+        const selectedOption = selectElement.value;
+        let prix = "";
+     
     if (livraisonType === "agence") {
   const prixagence = prixParwilayaagence[wilayaSelectionne];
   prixInput.value = prixagence !== undefined ? prixagence + " DA" : "Non desservie";
